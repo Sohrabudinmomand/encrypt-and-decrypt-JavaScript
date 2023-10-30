c9a697342a996fe701a244555e6ee5c5e010d9db
Sohrab momand <sohrabmomand.work@gmail.com>
2023-10-30 17:00:53 +0430

    the project is not complated yet

---
+++ script.js
@@ -0,0 +1,49 @@
+// console.log("sohrab momand");
+// console.log("Business");
+
+
+const sohrab = "Sohrab momand";
+console.log(sohrab);
+const cipher = window.btoa(sohrab);
+console.log(cipher);
+
+// console.log("-------------------------------")
+
+// const agianRealText = window.atob(cipher);
+// console.log(agianRealText);
+
+
+// console.log(window.btoa(sohrab));
+
+let encrypter;
+
+function encrypter() {
+    let storeString = document.querySelector('inputmaintext').value;
+
+    encrypter = window.btoa(storeString);
+    document.getElementById()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
