989ad593ff12141dbbb89d5575b92aa7b2d49264
Sohrab momand <sohrabmomand.work@gmail.com>
2023-10-31 17:45:26 +0430

    some changes

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 // console.log("Business");
 
 
-const sohrab = "Sohrab momand";
-console.log(sohrab);
-const cipher = window.btoa(sohrab);
-console.log(cipher);
+// const sohrab = "Sohrab momand";
+// console.log(sohrab);
+// const cipher = window.btoa(sohrab);
+// console.log(cipher);
 
 // console.log("-------------------------------")
 
@@ -15,17 +15,129 @@ console.log(cipher);
 
 // console.log(window.btoa(sohrab));
 
-let encrypter;
+// let encryptedData;
 
-function encrypter() {
-    let storeString = document.querySelector('inputmaintext').value;
+// function encrypter() {
+// let storeString = document.getElementById('inputRealText').value;
 
-    encrypter = window.btoa(storeString);
-    document.getElementById()
-}
+// let makingTheDataencrypt = storeString;
+
+// swtich(makingTheDataencrypt) {
+//         case 'sohrab':
+//     console.log('momand')
+
+
+// }
+
+// encryptedData = window.btoa(storeString);
+
+// if (storeString == 'sohrab') {
+//     let encryptedData = "vwhgvfd2u84yf9uw90ievxsssx0quvqhrgube09ru";
+// }
 
 
 
+
+
+
+
+
+
+
+// document.getElementById('inputEncryptedDate').value = encryptedData;
+
+// }
+
+// function decrypt() {
+//     let decryptedData = window.atob(encryptedData);
+//     document.getElementById('inputEncryptedDate').value = decryptedData;
+
+// }
+
+
+
+
+// const abcde = [['a', 'b', 'c', 'd', 'e', 'f',
+//     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+//     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+//     'y', 'z'],
+// ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+//     'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+//     'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']];
+
+// let enterdData = prompt('Enter a text');
+
+// let dataToencrypt = "";
+
+// if (enterdData === abcde[0[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]] || enterdData === abcde[1[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]]) {
+//     dataToencrypt += "akjbsdkv(^&*%&$986857KVJC";
+// } else {
+//     console.log("Error")
+// }
+
+
+// let enterdData = document.getElementById('inputRealText').value;
+// let enterdData = prompt('Enter a text');
+// let outputdecryptedData = "";
+
+// for (let i = 0; i < enterdData.length; i++) {
+
+//     switch (enterdData[i]) {
+//         case "a":
+//             outputdecryptedData += "jgf5%^";
+//             break;
+
+//         case "b":
+//             outputdecryptedData += "jsfoa++++%^";
+//             break;
+
+//         case "c":
+//             outputdecryptedData += "#$%^&";
+//             break;
+
+//         case "d":
+//             outputdecryptedData += "#$%^&";
+//             break;
+
+//         case "e":
+//             outputdecryptedData += "#$%^&";
+//             break;
+
+//         case "f":
+//             outputdecryptedData += "#$%^&";
+//             break;
+
+//         case "g":
+//             outputdecryptedData += "#$%^&";
+//             break;
+
+//         case "h":
+//             outputdecryptedData += "#$%^&";
+//             break;
+
+//         case "i":
+//             outputdecryptedData += "#$%^&";
+//             break;
+
+//         case "j":
+//             outputdecryptedData += "#$%^&";
+//             break;
+
+//         case "k":
+//             outputdecryptedData += "#$%^&";
+//             break;
+
+//         case "l":
+//             outputdecryptedData += "#$%^&";
+//             break;
+
+//         case "o":
+//             outputdecryptedData += "#$%^&";
+//             break;
+//     }
+// }
+
+// console.log(outputdecryptedData);
 
 
 
